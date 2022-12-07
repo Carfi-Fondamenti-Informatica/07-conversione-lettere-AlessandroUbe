fbf138a9e7bd42825b2f08c48c7668582ca94679
AlessandroUbe <115072327+AlessandroUbe@users.noreply.github.com>
2022-12-07 13:05:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
   char b=0;
     cin>>b;
-    if ( convert(b)){
+    if (convert(b)){
         cout<<b<<endl;
     }else{
         cout<<"errore"<<endl;
