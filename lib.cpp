dd2f5fadd890003bc2aff760d670b4c650e503ec
AlessandroUbe <115072327+AlessandroUbe@users.noreply.github.com>
2022-12-07 13:01:44 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -2,12 +2,12 @@
 // Created by Alessandro Ubertalli on 07/12/2022.
 //
 #include "lib.h"
-bool convert (char & a){
-    if (a>=65 && a<=90){
-        a +=32;
+bool convert (char & b){
+    if (b>=65 && b<=90){
+        b +=32;
         return true;
-    }else if(a>=97 && a<=122){
-        a -= 32;
+    }else if(b>=97 && b<=122){
+        b -= 32;
         return true;
     }
     return false;
